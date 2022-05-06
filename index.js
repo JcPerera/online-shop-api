c34d09fa5fb8c0efc3e3b5c6df1021bcf403f004
Janaka Perera <j.perera@optergy.com>
2022-05-06 14:23:01 +1000

    added helmet

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
+const helmet = require('helmet')
+
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 
-const cors = require("cors");
 
 dotenv.config();
-
+// need to initialize .env before mongoose, so we can use env variables inside db config
 require("./config/database").connect();
 
+const app = express();
 app.use(cors());
+app.use(helmet())
 app.use(express.json());
+
+// routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
